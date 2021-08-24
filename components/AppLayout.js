91386855507d082d9b2db3900a4ccca53a015541
happy-sonday <mrsmiler1106@gmail.com>
2021-08-24 15:27:54 +0900

    loginForm

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 const AppLayout = ({ children }) => {
+  const [loginFl, setLoginFl] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -28,7 +31,7 @@ const AppLayout = ({ children }) => {
       {/* NOTE:col끼리 guutter/2 좌우 padding */}
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {loginFl ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
