e59f845587bb8896393be49ba1e9b2d7084ddb22
happy-sonday <mrsmiler1106@gmail.com>
2021-11-11 00:32:12 +0900

    해시태그 등록 통신 구현

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Post, Image, Comment, User } = require("../models");
+const { Post, Image, Comment, User, Hashtag } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 const path = require("path");
 const multer = require("multer");
@@ -33,10 +33,19 @@ const upload = multer({
 
 router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
   try {
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
     const post = await Post.create({
       content: req.body.content,
       UserId: req.user.id,
     });
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map((tag) =>
+          Hashtag.findOrCreate({ where: { name: tag.slice(1).toLowerCase() } })
+        )
+      );
+      await post.addHashtags(result.map((v) => v[0]));
+    }
 
     // NOTE: front에서 key명은 반드시 일치해야한다.
     if (req.body.image) {
